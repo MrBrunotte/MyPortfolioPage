1e06b2a7f6c48aafcdebb7948dff63ce644dd8b6
mrbrunotte <mrbrunotte@gmail.com>
2020-08-14 12:01:19 +0200

    Add modal and style

--- static/js/custom.js
+++ static/js/custom.js
@@ -7,17 +7,17 @@ window.addEventListener("scroll", scrollFunction);
 
 function scrollFunction() {
   if (window.pageYOffset > 300) { // Show backToTopButton when scrolled 300px vertical
-    if(!backToTopButton.classList.contains("btnEntrance")) {
+    if (!backToTopButton.classList.contains("btnEntrance")) {
       backToTopButton.classList.remove("btnExit");
       backToTopButton.classList.add("btnEntrance");
       backToTopButton.style.display = "block";
     }
   }
   else { // Hide backToTopButton
-    if(backToTopButton.classList.contains("btnEntrance")) {
+    if (backToTopButton.classList.contains("btnEntrance")) {
       backToTopButton.classList.remove("btnEntrance");
       backToTopButton.classList.add("btnExit");
-      setTimeout(function() {
+      setTimeout(function () {
         backToTopButton.style.display = "none";
       }, 250);
     }
@@ -36,7 +36,7 @@ function smoothScrollBackToTop() {
   const distance = targetPosition - startPosition;
   const duration = 750;
   let start = null;
-  
+
   window.requestAnimationFrame(step);
 
   function step(timestamp) {
@@ -48,8 +48,30 @@ function smoothScrollBackToTop() {
 }
 
 function easeInOutCubic(t, b, c, d) {
-	t /= d/2;
-	if (t < 1) return c/2*t*t*t + b;
-	t -= 2;
-	return c/2*(t*t*t + 2) + b;
+  t /= d / 2;
+  if (t < 1) return c / 2 * t * t * t + b;
+  t -= 2;
+  return c / 2 * (t * t * t + 2) + b;
 };
+
+// The modal
+// Get the modal
+var modal = document.getElementById("myModal");
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
