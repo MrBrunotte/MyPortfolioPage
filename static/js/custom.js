301204cc2df10508da25b4affd83802d77e07c1e
mrbrunotte <mrbrunotte@gmail.com>
2020-09-01 00:07:10 +0200

    Add hamburger bar mobile

--- static/js/custom.js
+++ static/js/custom.js
@@ -74,4 +74,31 @@ window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-} 
+}
+
+// burger menu
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav_links');
+  const navLinks = document.querySelectorAll('.nav_links li')
+
+  // Toggle nav
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav_active');
+
+    // Animate links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = ''
+      }
+      else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1}s`;
+      }
+      //console.log(index / 7 + 1);
+    });
+    //Burger Animation
+    burger.classList.toggle('toggle');
+  });
+}
+
+navSlide();
